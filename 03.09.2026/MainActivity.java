01425eb94877307fdacb574d2b2e5efd6a6a2ea4
Wiktor Waligórski <145098483+WiktorWaligorski@users.noreply.github.com>
2026-09-06 18:39:49 +0200

    Dokończenie

--- 03.09.2026/MainActivity.java
+++ 03.09.2026/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.egzamin_mobilki_1;
+package com.example.myapplication;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,19 +12,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.sql.Array;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int dice_Throw(){
+    public int dice_Throw(ImageView[] images, Drawable[] drawables){
         Random random = new Random();
         int Rolls[] = new int[5];
-
         for (int i = 0; i < 5; i++){
             Rolls[i] = random.nextInt(6) + 1;
+            images[i].setImageDrawable(drawables[Rolls[i] - 1]);
         }
         return calculate_Score(Rolls);
     }
@@ -56,12 +58,44 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        ImageView d1 = findViewById(R.id.dice_1);
+        ImageView d2 = findViewById(R.id.dice_2);
+        ImageView d3 = findViewById(R.id.dice_3);
+        ImageView d4 = findViewById(R.id.dice_4);
+        ImageView d5 = findViewById(R.id.dice_5);
+
+        ImageView[] images = {d1, d2, d3, d4, d5};
+        Drawable[] diceDrawables = {
+                getDrawable(R.drawable.k1),
+                getDrawable(R.drawable.k2),
+                getDrawable(R.drawable.k3),
+                getDrawable(R.drawable.k4),
+                getDrawable(R.drawable.k5),
+                getDrawable(R.drawable.k6),
+                getDrawable(R.drawable.question)};
+
         Button ThrowButton = findViewById(R.id.throw_but);
+        Button Reset = findViewById(R.id.reset_button);
+
         TextView wynik = findViewById(R.id.score_cur);
+        TextView total = findViewById(R.id.total_score);
+
+        AtomicInteger score = new AtomicInteger();
 
         ThrowButton.setOnClickListener(v -> {
-            int score = dice_Throw();
-            wynik.setText("Wynik tego losowania: " + score);
+            int current_roll = dice_Throw(images, diceDrawables);
+            wynik.setText("Wynik tego losowania: " + current_roll);
+            score.set(score.get() + current_roll);
+            total.setText("Wynik gry: " + score);
+        });
+
+        Reset.setOnClickListener(v -> {
+            score.set(0);
+            wynik.setText("Wynik tego losowania: " + 0);
+            total.setText("Wynik gry: " + score);
+            for(ImageView image : images){
+                image.setImageDrawable(diceDrawables[6]);
+            }
         });
 
     }
